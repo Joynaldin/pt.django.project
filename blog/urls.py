771cafd36a8842ee64ffdf32b4b1ccfb7286f846
joynaldin <joynalabedin014057@gmail.com>
2023-10-20 21:04:29 +0600

    create navbar and header

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home_page"),
-    path('about', views.about, name="about_page"),
+    path('', views.home, name="home page"),
+    path('about', views.about, name="about page"),
 
 ]

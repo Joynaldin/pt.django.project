20c97a791344a2b0fc548c3e57b48e75c9555834
joynaldin <joynalabedin014057@gmail.com>
2023-10-27 21:33:54 +0600

    create registation form

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home page"),
+    path('', views.home, name="blog-home"),
     path('about', views.about, name="about page"),
 
 ]
